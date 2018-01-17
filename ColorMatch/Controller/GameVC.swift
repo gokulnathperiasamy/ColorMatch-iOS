9c648b26ff2f701b669572ecdc989cda877854ec
Gokul Nath KP <gokulnath.kpgn@gmail.com>
2018-01-17 11:30:39 +0530

    Add time and score views.

--- ColorMatch/Controller/GameVC.swift
+++ ColorMatch/Controller/GameVC.swift
@@ -14,6 +14,8 @@ class GameVC: BaseVC {
     @IBOutlet weak var viewB: UIView!
     @IBOutlet weak var ctaNo: UIButton!
     @IBOutlet weak var ctaYes: UIButton!
+    @IBOutlet weak var viewTime: UIView!
+    @IBOutlet weak var viewScore: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +29,8 @@ class GameVC: BaseVC {
         applyCornerRadius(view: viewB)
         applyCornerRadius(view: ctaNo)
         applyCornerRadius(view: ctaYes)
+        applyCornerRadius(view: viewTime)
+        applyCornerRadius(view: viewScore)
     }
 
     override func didReceiveMemoryWarning() {
